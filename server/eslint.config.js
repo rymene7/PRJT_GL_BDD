feb0c31fa47b599df7bab19ebab50ac121f2ffd1
rymene <163442170+rymene7@users.noreply.github.com>
2024-12-22 14:54:39 +0100

    Create eslint.config.js

---
+++ server/eslint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+export default [
+  {
+    files: ['*.js'],
+    extends: ['eslint:recommended'],
+    rules: {
+      // Add your custom rules here
+    },
+    env: {
+      node: true,
+      es6: true,
+    },
+  },
+];
