c97c791fbb657a5e9cc79d5630c1a0ea4c51ce6a
rymene <163442170+rymene7@users.noreply.github.com>
2024-12-22 14:18:16 +0100

    Update App.spec.jsx

--- client/src/__tests__/App.spec.jsx
+++ client/src/__tests__/App.spec.jsx
@@ -1,5 +1,8 @@
 // src/__tests__/App.test.js
 
+// Import the testing functions from Jest
+const { test, expect } = require('@jest/globals');
+
 test('demo', () => {
   expect(true).toBe(true);
 });
