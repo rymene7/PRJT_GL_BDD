c0c976dfb18a288c17c3243447ca0511eaacca7a
rymene <163442170+rymene7@users.noreply.github.com>
2024-12-22 15:09:14 +0100

    Update eslint.config.js

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,16 +1,18 @@
 // eslint.config.js
-import eslintConfigRecommended from 'eslint-config-recommended';
-
 export default [
-  eslintConfigRecommended,
   {
     files: ['*.js'],
     rules: {
       // Add your custom rules here
+      'no-unused-vars': 'warn', // Example rule
+      'semi': ['error', 'always'], // Example rule
     },
     env: {
       node: true,
       es6: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2020,
     },
   },
 ];
